b9cbdd7f2d3ec96deffdaa19be189689c5f78417
ipzk241_myav <ipzk241_myav@student.ztu.edu.ua>
2025-01-25 20:44:29 +0200

    додав лічильник

--- ipzk241MYAV/script.js
+++ ipzk241MYAV/script.js
@@ -1,8 +1,12 @@
 let TaskName = document.getElementById("createTask");
-
+var i=0;
 TaskName.addEventListener("click", () => {
     let TaskValue = document.getElementById("TaskValue").value;  
-    let newTask = document.createElement("li");
-    newTask.textContent = TaskValue;  
-    document.getElementById("TaskList").appendChild(newTask); 
+    let newTask = document.createElement("li"); 
+    let numberTask = `Завдання ${i}: `;
+    newTask.textContent = numberTask + TaskValue; 
+
+    document.getElementById("TaskList").appendChild(newTask);
+    i++; 
+    document.getElementById("TaskValue").value = "";
 });
