7319df9f0937eba7c9e0ec3d734e57499032b117
ipzk241_myav <ipzk241_myav@student.ztu.edu.ua>
2025-01-26 17:29:39 +0200

    додав перевірку при додаванні тексту

--- ipzk241MYAV/script.js
+++ ipzk241MYAV/script.js
@@ -1,7 +1,10 @@
 let TaskName = document.getElementById("createTask");
 var i=0;
 TaskName.addEventListener("click", () => {
+    
     let TaskValue = document.getElementById("TaskValue").value;  
+    if(TaskValue)
+    {
     let newTask = document.createElement("li"); 
     let numberTask = `Завдання ${i}: `;
     newTask.textContent = numberTask + TaskValue; 
@@ -9,4 +12,9 @@ TaskName.addEventListener("click", () => {
     document.getElementById("TaskList").appendChild(newTask);
     i++; 
     document.getElementById("TaskValue").value = "";
+    }
+    else
+    {
+        alert("ви не ввели завдання");
+    }
 });
