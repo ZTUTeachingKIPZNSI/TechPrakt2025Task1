bef76d92bc349aa18b4e95036dd22f4a6bec5e50
OlenchukArtem <vt232_oao@student.ztu.edu.ua>
2025-01-27 12:18:53 +0200

    внесені зміни у файл jsdemo

--- jsdemo/js/main.js
+++ jsdemo/js/main.js
@@ -11,7 +11,7 @@ let arr = [
     },
     {
         "name": "Sergiy",
-        "age": 19,
+        "age": 18,
         "title": "description"
     }, {
         "name": "Sergiy",
@@ -35,7 +35,7 @@ let arr = [
         "title": "description"
     },{
         "name": "Sergiy",
-        "age": 19,
+        "age": 20,
         "title": "description"
     },
 ];
