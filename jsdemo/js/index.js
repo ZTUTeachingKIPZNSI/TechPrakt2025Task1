77185be993ed6b2d3bfa6958c053539df97a9ced
BohdanHnidyi <vtk241_gbs@student.ztu.edu.ua>
2025-02-14 21:02:05 +0200

    Some changes in folder jsdemo

--- jsdemo/js/index.js
+++ jsdemo/js/index.js
@@ -32,14 +32,11 @@ class Admin extends User {
 const user = new User('admin', '123456');
 
 let panelParams = {
-    //width : 300,
     title : 'Title',
     height : 200,
     age : 30
 };
 
-let {title, height} = panelParams;
-let [id, title, count, price] = [1, 'title', 10, 10,5];
 let x = 1;
 let y = 2;
 [x, y] = [y, x];
@@ -51,13 +48,3 @@ function func2(a, b, c) {
     console.log(b);
     console.log(c);
 }
-/*let arr2 = [1,2,3,4,5];
-func2(...arr2);*/
-
-/*
-let myf = user.getLogin;
-console.log(myf.bind(user));
-
-console.log(user.getLogin());
-const admin = new Admin('admin', '123456', 'all');
-*/
--- jsdemo/js/main.js
+++ jsdemo/js/main.js
@@ -13,31 +13,7 @@ let arr = [
         "name": "Sergiy",
         "age": 19,
         "title": "description"
-    }, {
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    }, {
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    },{
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    },{
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    },{
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    },{
-        "name": "Sergiy",
-        "age": 19,
-        "title": "description"
-    },
+    }, 
 ];
 
 class UserPanels {
